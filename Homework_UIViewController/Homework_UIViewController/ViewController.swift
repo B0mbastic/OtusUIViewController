3f9549a649661443567bbe3aae016b665c2380c5
B0mbastic <100468563+B0mbastic@users.noreply.github.com>
2023-01-30 23:45:55 +0800

    Add files via upload

---
+++ Homework_UIViewController/Homework_UIViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework_UIViewController
+//
+//  Created by Александр Ковбасин on 30.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
